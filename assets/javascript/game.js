d2fa363ef0204ae184660e245e26b6e438572e7f
Your Name <you@example.com>
2018-09-08 19:04:59 -0400

    saving

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+console.log("hi");
+
+var randomWordArray = ["hibiscus", "dogwood", "pine", "ficus"];
+//var randomWord = randomWordArray[Math.floor(Math.random() * randomWordArray.length)];
+
+for (var i = 0; i < randomWordArray.length; i++){
+    console.log(randomWordArray[i]);
+}
+
+
+
+
